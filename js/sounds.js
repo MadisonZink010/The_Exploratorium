f22743613641c2c9d4669a1cad2a7f7a285fec20
Madison Zink <111449888+MadisonZink010@users.noreply.github.com>
2023-03-20 17:16:39 +0000

    Mon, Mar 20, 2023, 5:16 PM +00:00

--- js/sounds.js
+++ js/sounds.js
@@ -2,5 +2,9 @@ const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
 
 sounds.forEach(sound => {
     const btn = document.createElement('button')
-    btn
+    btn.classList.add('btn')
+    btn.innerText = sound
+    btn.addEventListener('click' () => {
+        
+    })
 })
