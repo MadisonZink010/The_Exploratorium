e5d698a0aab9e0e49eb95f864552e60e55808110
Madison Zink <111449888+MadisonZink010@users.noreply.github.com>
2023-03-24 16:44:49 +0000

    Fri, Mar 24, 2023, 4:44 PM +00:00

---
+++ js/typing.js
@@ -0,0 +1,16 @@
+const textEl = document.getElementById('text')
+const speedEl = document.getElementById('speed')
+const text = 'We Love Programming!'
+let idx = 1
+let speed = 300
+
+writeText()
+
+function writeText() {
+    textEl.innerText = text.slice(o,idx)
+    idx++
+    if (idx > text.length) {
+        idx = 1
+    }
+    setTimeout(writeText, speed)
+}
